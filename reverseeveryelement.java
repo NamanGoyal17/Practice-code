5dfe01be311f25bd477fa9e1e350940753409962
Naman Goyal <113236630+NamanGoyal17@users.noreply.github.com>
2023-09-09 11:35:19 +0530

    Add files via upload

---
+++ reverseeveryelement.java
@@ -0,0 +1,21 @@
+package final_exam;
+import java.util.*;
+public class reverseeveryelement {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();
+		char arr[] = str.toCharArray();
+		for(int i =0 ;i<arr.length;i++) {
+		
+			char brr[] = ;
+			for(int j= brr.length-1;j>=0;j--) {
+				System.out.print(brr[i]);
+			}
+			System.out.print(" ");
+			
+		}
+
+	}
+
+}
